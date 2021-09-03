ec1e791d90a225cc5f5f120ce1764fa2c66755c2
unknown <sfriede5@jh.edu>
2021-09-02 22:11:13 -0400

    fixed it!

--- oose_lec2/src/main/java/Main.java
+++ oose_lec2/src/main/java/Main.java
@@ -2,11 +2,19 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) throws IOException {
+        final String BASE_URL = "https://sis.jhu.edu/api/classes";
+        final String KEY = "DLMHOuFmGlh1NDUlp8jfgklV6zkvlbpk";
 
-    //store API as new environment variable (System.getenv(name, "name"));
-    final String BASE_URL = "blah";  //from SIS
-    final String KEY = System.getenv("API");
-    System.out.println("hi"); ///hello
+        String endpoint = BASE_URL + "/codes/schools?Key=" + KEY;
 
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(endpoint)
+                .build();
+        Response response = client.newCall(request).execute();
+        System.out.println(response.body().string());
+    }
 }
